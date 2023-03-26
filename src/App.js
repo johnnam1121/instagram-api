b68597b568d875c577ec4fda891617d6a34f3dd0
johnnam1121 <johnnam93@gmail.com>
2023-03-26 17:39:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import InstagramCalls from './InstagramCalls';
 function App() {
   return (
     <div>
+      <h1>Instagram Test</h1>
       <InstagramCalls token={process.env.REACT_APP_INS_TOKEN} limit={12} />
     </div>
   );
